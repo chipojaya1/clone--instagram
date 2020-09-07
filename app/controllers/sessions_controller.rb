235b37cb4261e1bd2d0abb305552c3611c001b5e
chipojaya1 <jaya@diveintocode.jp>
2020-09-07 09:04:39 +0000

    login funtion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
-end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+
+    else
+
+    end
+  end
