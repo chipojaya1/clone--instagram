aa733e681d5444f76a5feb48284e4242c3da6469
chipojaya1 <jaya@diveintocode.jp>
2020-09-07 09:08:41 +0000

    login funtion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
 
     else
-
+      flash.now[:notice] = 'failed to log in'
+      render :new
     end
   end
