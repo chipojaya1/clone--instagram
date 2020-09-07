25bf1653833d4aa18fb7f77867a73100cc554527
chipojaya1 <jaya@diveintocode.jp>
2020-09-07 06:23:12 +0000

    user detail page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      redirect_to user_path(@user.id)
     else
-      render 'new'
+      render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.order(created_at: "DESC")
   end
 
   private
