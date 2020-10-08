8f4bbb60372d042e798f91345c50fa37f3eb3e84
chipojaya1 <jaya@diveintocode.jp>
2020-10-08 06:41:54 +0000

    japanese timezone and other

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Cloneinsta
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-    config.i18n.default_locale = :en
+    config.load_defaults 5.1
+    config.generators do |g|
     # Settings in config/environments/* take precedence over those specified here.
+      g.assets false
+      g.helper false
+    end
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
