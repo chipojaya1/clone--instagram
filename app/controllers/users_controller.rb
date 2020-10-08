48686310b464d0e19599c48656aefb41a9ac3efa
chipojaya1 <jaya@diveintocode.jp>
2020-10-08 06:43:58 +0000

    reconfiguring user interface

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all.order(created_at: "DESC")
@@ -14,43 +14,39 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if params[:back]
-      render :new
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id), notice: 'User was successfully created'
     else
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to @user, notice: 'Acoount created'
-      else
-        render :new
-      end
+      render :new
     end
-  end
-
-  def edit
-    @user = User.find(params[:id])
   end
 
   def show
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def update
-    if @user.id == current_user.id
-      if @user.update(user_params)
-        redirect_to @user, notice: 'Acoount updated'
-      else
-        render :edit
-      end
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), success: 'User was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  def confirm
-    @user = User.new(user_params)
-    render :new if @user.invalid?
+  def favourite
+    @user = User.find(params[:id])
+    @favorites = current_user.favorite_posts.all
   end
 
-  def icon
-    @posts = current_user.favorite_posts
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to new_user_path, notice: 'User was successfully destroyed.'
   end
 
   private
