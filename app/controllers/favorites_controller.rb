5e0d63baaf375dd10ca3f4eac0eec8ea285bee0c
chipojaya1 <jaya@diveintocode.jp>
2020-10-08 06:45:14 +0000

    reconfiguring posting function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,11 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(post_id: params[:post_id])
-    redirect_to posts_url, notice: "#you have liked {favorite.post.user.name}'s post"
+    redirect_to posts_url, notice: "#you have liked #{favorite.post.user.name}'s post"
   end
 
   def destroy
-    post_id = current_user.favorites.find_by(id: params[:id]).post_id
-    favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to posts_url, notice: "#you have deleted like for {favorite.post.user.name}'s post"
-  end
-
-  def show
-    @favorites = current_user.favorites.order(created_at: "DESC")
+    favorite = current_user.favorites.destroy.(post_id: params[:post_id])
+    redirect_to @post, notice: "#you have deleted like for #{favorite.post.user.name}'s post"
   end
 end
