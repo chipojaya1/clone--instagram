585a8da3fc7a9765a504e9e80ada5c3034e49df8
chipojaya1 <jaya@diveintocode.jp>
2020-10-12 04:25:37 +0000

    debugging for the last time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'instgramc.herokuapp.com' }
-  
-  confi.action_mailer.delivery_method = :letter_opener_web
 
+  config.action_mailer.delivery_method = :letter_opener_web
 end
